bd016604486960ba5d132577366b32b97ded2d90
wellwind <wellwind@mail2000.com.tw>
2017-03-01 22:03:08 +0800

    Two way binding + NativeScriptFormsModule

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { NativeScriptFormsModule } from "nativescript-angular/forms";
 
 import { AppComponent } from "./app.component";
 import { ActionBarComponent } from "./action-bar/action-bar.component";
@@ -7,7 +8,10 @@ import { ActionBarComponent } from "./action-bar/action-bar.component";
 @NgModule({
   declarations: [AppComponent, ActionBarComponent],
   bootstrap: [AppComponent],
-  imports: [NativeScriptModule],
+  imports: [
+    NativeScriptModule,
+    NativeScriptFormsModule
+  ],
   schemas: [NO_ERRORS_SCHEMA],
 })
 export class AppModule {}
