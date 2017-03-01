a6a63b2bbb5b31604788b46736f35adf93a9cf09
wellwind <wellwind@mail2000.com.tw>
2017-03-01 22:34:05 +0800

    Import NativeScriptHttpModule

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { NativeScriptFormsModule } from "nativescript-angular/forms";
+import { NativeScriptHttpModule } from "nativescript-angular/http";
 
 import { AppComponent } from "./app.component";
 import { ActionBarComponent } from "./action-bar/action-bar.component";
@@ -10,7 +11,8 @@ import { ActionBarComponent } from "./action-bar/action-bar.component";
   bootstrap: [AppComponent],
   imports: [
     NativeScriptModule,
-    NativeScriptFormsModule
+    NativeScriptFormsModule,
+    NativeScriptHttpModule
   ],
   schemas: [NO_ERRORS_SCHEMA],
 })
