13f04c1cd776cc3d6fdcf6f773472b35485fd4d0
wellwind <wellwind@mail2000.com.tw>
2017-03-03 21:37:35 +0800

    ListView

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,8 +1,16 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "list",
-  templateUrl: "pages/list/list.html",
-  styleUrls: ["pages/list/list-common.css", "pages/list/list.css"]
+  selector: 'app-list',
+  templateUrl: 'pages/list/list.html',
+  styleUrls: ['pages/list/list-common.css', 'pages/list/list.css']
 })
-export class ListComponent {}
+export class ListComponent implements OnInit {
+  groceryList: Array<Object> = [];
+
+  ngOnInit() {
+    this.groceryList.push({ name: 'Apples' });
+    this.groceryList.push({ name: 'Bananas' });
+    this.groceryList.push({ name: 'Oranges' });
+  }
+}
