a6e8bd23369ddc335d8f7574981d401e55551739
wellwind <wellwind@mail2000.com.tw>
2017-03-03 21:54:29 +0800

    Add loading indicator: ActivityIndicator

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -8,15 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class ListComponent implements OnInit {
   groceryList: Array<Object> = [];
   grocery: string;
+  isLoading = false;
 
   ngOnInit() {
-    this.groceryList.push({ name: 'Apples' });
-    this.groceryList.push({ name: 'Bananas' });
-    this.groceryList.push({ name: 'Oranges' });
+    this.isLoading = true;
+    setTimeout(() => {
+      this.groceryList.push({ name: 'Apples' });
+      this.groceryList.push({ name: 'Bananas' });
+      this.groceryList.push({ name: 'Oranges' });
+      this.isLoading = false;
+    }, 500);
+
   }
 
   add() {
-    this.groceryList.push({name: this.grocery });
+    this.groceryList.push({ name: this.grocery });
     this.grocery = '';
   }
 }
